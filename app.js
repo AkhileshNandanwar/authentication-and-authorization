8097065828d89859d49cad8c63c030a1ece8a1bb
AkhileshNandanwar <akhileshnandanwar751@gmail.com>
2025-01-07 23:39:46 +0530

    added jwt

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const cookieParser = require("cookie-parser");
 const express=require("express")
 const app=express();
-app.use(cookieParser())
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+
+app.use(cookieParser());
+
+
+/*
+app.get("/",(req,res)=>{
+//copy this code from npm-package-bcrypt
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("akhilesh", salt, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+        
+    });
+});
+})
+
+*/
 
 app.get("/",(req,res)=>{
-res.cookie("name","akhilesh")
-res.send("done");
+    let token=jwt.sign({email:"akhilesh@gmail.com"},"secret")
+    res.cookie("token",token);
+    console.log(token);
+    res.send("done")
+    
 })
 
 app.get("/read",(req,res)=>{
-    console.log(req.cookies);
-    res.send("read page")
-
+    let data=jwt.verify(req.cookies.token,"secret");
+    console.log(data);
     
-    })
 
+})
 app.listen(3000);
